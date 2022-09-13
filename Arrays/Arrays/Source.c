2e4ab8c68759936dc4f66811e0a7b6a8139c05ea
Antoan Stefanov <antoine.stefanov@gmail.com>
2022-09-13 11:29:38 +0300

    Arrays-Max-Min-51

--- Arrays/Arrays/Source.c
+++ Arrays/Arrays/Source.c
@@ -28,11 +28,41 @@ int main() {
 		arr[i] = malloc(sizeof(int) * cols);
 	}
 
+	int* MinValuesArr = malloc(sizeof(int)*cols);
+
+	printf("Inserting goes by column.\n");
+	for (int j = 0; j < cols; j++) {
+		int minValue = INT_MAX;
+		for (int i = 0; i < rows; i++) {
+			printf("Element[%d][%d]= ", i,j);
+			scanf_s("%d", &arr[i][j]);
+			if (arr[i][j] < minValue) {
+				minValue = arr[i][j];
+			}
+		}
+		MinValuesArr[j] = minValue;
+	}
+
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			printf("Element: %d\n", arr[i][j]);
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < cols; i++) {
+		printf("Min Value: %d\n", MinValuesArr[i]);
+	}
+
+	int maxOfMins = MinValuesArr[0];
+	int maxOfMinsColumnNumber = -1;
+	for (int i = 1; i < cols; i++) {
+		if (MinValuesArr[i] > maxOfMins) {
+			maxOfMins = MinValuesArr[i];
+			maxOfMinsColumnNumber = i + 1;
 		}
 	}
+	printf("Max Value of Mins: %d, Column Number = %d\n", maxOfMins, maxOfMinsColumnNumber);
 
 	freeArray(arr, rows);
 
