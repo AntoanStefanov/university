9ba91f9dac80c53c71574c2537d728d9df2c9af6
Antoan Stefanov <antoine.stefanov@gmail.com>
2022-09-19 21:43:06 +0300

    inserting constants in C way

--- termWork/termWork/Source.c
+++ termWork/termWork/Source.c
@@ -2,12 +2,19 @@
 #include <string.h>
 #include <stdlib.h> 
 
+#define MAX_NAME_CHARS 31
+#define MAX_EGN_CHARS 11
+#define MAX_FACULTY_NUMBER_CHARS 7
+#define MAX_GRADES_NUMBER 10
+
+
 typedef struct {
-	char name[31];
-	char EGN[11];
-	char facultyNumber[7];
-	float grades[10];
+	char name[MAX_NAME_CHARS];
+	char EGN[MAX_EGN_CHARS];
+	char facultyNumber[MAX_FACULTY_NUMBER_CHARS];
+	float grades[MAX_GRADES_NUMBER];
 	float averageGrade;
+	// gender ?
 } student;
 
 
@@ -22,6 +29,27 @@ int readNumberOfStudents(int* numberOfStudents) { // Pointer arg, bcs we need to
 	return 1;
 }
 
+void readStudentsInformation(student* students, int numberOfStudents) {
+	
+	for (int currentStudentNum = 0; currentStudentNum < numberOfStudents; currentStudentNum++) {
+		printf("Student Number: #%d...\n", currentStudentNum + 1);
+
+		// %(30|10|6)s ==> WIDTH SPECIFICATION || (unsigned)strlen(students[currentStudentNum].nam -> BUFFER SIZE.
+
+		printf("---Student's Name: "); scanf_s("%30s", students[currentStudentNum].name, MAX_NAME_CHARS);
+
+		printf("---Student's EGN : "); scanf_s("%10s", students[currentStudentNum].EGN, MAX_EGN_CHARS);
+
+		printf("---Student's Faculty Number: "); scanf_s("%6s", students[currentStudentNum].facultyNumber, MAX_FACULTY_NUMBER_CHARS);
+
+		printf("Test");
+
+		// students[currentStudentNum] = student;
+	}
+
+	printf("asd");
+}
+
 
 int main() {
 	int numberOfStudents;
@@ -32,6 +60,7 @@ int main() {
 
 	student* students = malloc(sizeof(student) * numberOfStudents);
 
+	readStudentsInformation(students, numberOfStudents);
 
 
 
