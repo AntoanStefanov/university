b2134e58bc75d2fc7e047350b4f5768135e2a48c
Antoan Stefanov <antoine.stefanov@gmail.com>
2022-09-20 14:30:09 +0300

    Menu created

--- termWork/termWork/Source.c
+++ termWork/termWork/Source.c
@@ -13,8 +13,6 @@ typedef struct {
 	char EGN[MAX_EGN_CHARS];
 	char facultyNumber[MAX_FACULTY_NUMBER_CHARS];
 	float grades[MAX_GRADES_NUMBER];
-	float averageGrade;
-	// gender ?
 } student;
 
 
@@ -35,8 +33,7 @@ void readStudentsInformation(student* students, int numberOfStudents) {
 	for (int currentStudentNum = 0; currentStudentNum < numberOfStudents; currentStudentNum++) {
 		printf("Student Number: #%d...\n", currentStudentNum + 1);
 
-		// %(30|10|6)s ==> WIDTH SPECIFICATION || (unsigned)strlen(students[currentStudentNum].nam -> BUFFER SIZE.
-
+		// %(30|10|6)s ==> WIDTH SPECIFICATION || MAX_(NAME|EGN|FACULTY_NUMBER)_CHARS -> BUFFER SIZE.
 		printf("---Student's Name: "); scanf_s("%30s", students[currentStudentNum].name, MAX_NAME_CHARS);
 
 		printf("---Student's EGN : "); scanf_s("%10s", students[currentStudentNum].EGN, MAX_EGN_CHARS);
@@ -58,16 +55,37 @@ void readStudentsInformation(student* students, int numberOfStudents) {
 			token = strtok_s(NULL, ", ", &next_token);
 		}
 		// !!!!!!!!!!!!!!!!!!! SPLIT GRADES' STRING !!!!!!!!!!!!!!!!!!!!!!
-
 	}
+}
 
-	printf("asd");
+void displayMenu() {
+	printf("\t\tMENU\n");
+	printf("1.---Create file with student's information.\n");
+	printf("2.---Calculate Average Grade For Every Student.\n");
+	printf("3.---Display Men's Faculty Numbers With More Than 5 Very Bad Grades.\n");
+	printf("4.---Display Menu.\n");
+	printf("5.---EXIT\n");
+}
+
+void createFile() {
+	printf("Not working at moment\n");
 }
 
 
+void calculateAvgGrades() {
+	printf("Not working at moment\n");
+
+}
+
+
+void displayMenWithBadGrades() {
+	printf("Not working at moment\n");
+
+}
+
 int main() {
 	int numberOfStudents;
-
+	int menuNumber;
 	if (!readNumberOfStudents(&numberOfStudents)) {
 		return 0;
 	}
@@ -75,15 +93,22 @@ int main() {
 	student* students = malloc(sizeof(student) * numberOfStudents);
 
 	readStudentsInformation(students, numberOfStudents);
+	displayMenu();
 
+	do {
+		printf("Choose from 1-5 Options: "); scanf_s("%d", &menuNumber); (void)getchar();
 
+		switch (menuNumber) {
+		case 1: createFile(); break;
+		case 2: calculateAvgGrades(); break;
+		case 3: displayMenWithBadGrades(); break;
+		case 4: displayMenu(); break;
+		case 5: printf("Goodbye! Have a nice day!\n"); break;
+		default: printf("No such function.\n");
+		}
 
+	} while (menuNumber != 5);
 
-	
-
-	
-
-	
 
 
 	return 0;
