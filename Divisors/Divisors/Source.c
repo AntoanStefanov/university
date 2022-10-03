a6541e89b788258ccb58d8645f017a9ee44425ac
Antoan Stefanov <antoine.stefanov@gmail.com>
2022-10-03 14:47:05 +0300

    string task

--- Divisors/Divisors/Source.c
+++ Divisors/Divisors/Source.c
@@ -1,5 +1,9 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
 
 void divisors(unsigned n, size_t* z, unsigned* array) {
     *z = 0;
@@ -12,12 +16,30 @@ void divisors(unsigned n, size_t* z, unsigned* array) {
 }
 
 
-#include <stdbool.h>
+//  do not allocate memory for return string
+//  assign the value to the pointer "result"
 
-bool is_pangram(const char* str_in) {
+void spin_words(const char* sentence, char* result) {
+    char* next_token = NULL;
+    char* token;
+    token = strtok_s(sentence, " ", &next_token);
+    int indexCounter = 0;
+    while (token) {
+        if (strlen(token) >= 5) {
+            _strrev(token);
+        }
+        for (int i = 0; i < strlen(token); i++) {
+            result[indexCounter] = token[i];
+            indexCounter++;
+        }
+        result[indexCounter] = ' ';
+        indexCounter++;
+        token = strtok_s(NULL, " ", &next_token);
 
-    //  <----  hajime!
-
+    }
+    result[strlen(result) - 1] = '\0';
+    printf("%s", result);
+    printf("A");
 }
 
 int main() {
@@ -29,12 +51,14 @@ int main() {
     divisors(12, &z, arr); // results in {2, 3, 4, 6}
     divisors(25, &z, b); // results in {5}
     divisors(13, &z, c); // results in NULL
-    printf("check");
+    printf("check\n");
     // divisors func
 
-    // is_pangram func
-    // ...
-    // is_pangram func
+    // spin_words func
+    char res[3000];
+    char str[] = "Hey fellow warriors";
+    spin_words(str, res);
+    // spin_words func
 
     return 0;
 }
