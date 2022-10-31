96d97698d0a1d67ce79773be0616f9a6179bbb25
Antoan Stefanov <antoine.stefanov@gmail.com>
2022-10-31 18:43:21 +0200

    adding functionality

--- Replace_With_Alphabet_Position/Replace_With_Alphabet_Position/Source.c
+++ Replace_With_Alphabet_Position/Replace_With_Alphabet_Position/Source.c
@@ -2,7 +2,17 @@
 
 // returned string has to be dynamically allocated and will be freed by the caller
 char* alphabet_position(const char* text) {
+	for (int i = 0; i < 5; i++) {
+		char ch = text[i];
+	}
+	char* numbers = "1 2 3";
 
-	return NULL;
+	return numbers;
 }
 
+int main() {
+	const char* input = "The sunset sets at twelve o' clock.";
+	char* ptr = alphabet_position(&input);
+	free(ptr);
+	return 0;
+}
