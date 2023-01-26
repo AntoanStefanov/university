2b89fc699ac83520a6858b94397ae13b9a2981ea
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-26 19:19:09 +0200

    constructor

--- second_semester/first_steps_OOP/first_steps_OOP/Source.cpp
+++ second_semester/first_steps_OOP/first_steps_OOP/Source.cpp
@@ -1,43 +1,59 @@
 #include <iostream>
+#include <format>
 
-// using namespace std; -> bad practice. Instead do include the things u need or write everytime "std::...."
+// using namespace std; -> bad practice. Instead, include only the things u need or write everytime "std::...."
 // https://www.youtube.com/watch?v=etQX4Mme2f4|https://www.geeksforgeeks.org/using-namespace-std-considered-bad-practice/
 
 using std::string;
 using std::cout;
 using std::endl;
+using std::format;
 
 // used JS naming convetions.
 // class -> PascalCase (ExampleClass)
-// members -> camelCase (exampleVariable)
+// members(properties and methods) -> camelCase (exampleVariable)
 
 // class Employee { body }
 class Employee {
 	// EVERYTHING INSIDE CLASS IN C++ is PRIVATE BY DEFAULT !!
 	// we have 3 access modifiers: public, protected, private. (private by default).
 	// 
-	// private: NOT ACCESSIBLE OUTSIDE THE CLASS.
+	// private: NOT ACCESSIBLE OUTSIDE THE CLASS. (by default)
 	// protected: NOT ACCESSIBLE OUTSIDE THE CLASS AND ITS DERIVATIVES CLASSES.
 	// public: ACCESSIBLE EVERYWHERE.
-	// 
-	// Add Members of the class (members are its attributes/properties/ and its behaviour/methods/)
+public:
+
+	// properties
 	string name;
 	string company;
 	int age;
-	// above are 3 attributes(properties)
+
+	// methods
+
+	// 1. constructor method does NOT have a return type.
+	// 2. constructor has the same name as the class that it belongs to. /class Employee -> constructor Employee/.
+	// 3. constructor must be public./at this level in our knowledge./
+	Employee(string employeeName, string employeeCompany, int employeeAge) {
+		// assign parameters to the properties.
+		name = employeeName;
+		company = employeeCompany;
+		age = employeeAge;
+	}
+
+	void introduce() {
+		cout << format("Name - {}. Company - {}. Age - {}", name, company, age) << endl;
+	}
+
 };
 
 int main() {
-	// how do we create a variable of type int?
-	int x = 3; // data type | variable name = value;
-	// above, we created a variable of type int.
+	// Class objectName = Constructor(args...);
+	Employee employee1 = Employee("Antoan", "Company", 22); // calling the class constructor. Constructing the object itself.
+	employee1.introduce();
 
-	// how do we create an object/instance of this class?
-	// same goes for a variable of type Employee /int example above/
+	Employee employee2 = Employee("Samantha", "Company", 19);
+	employee2.introduce();
 
-	// a variable of type Employee
-	Employee employee1; // data type(user-defined type) | variable name
-	// above, we successfully have created a variable of type Employee. 
-	// (employee1 is an object/instance of class Employee)
-	employee1.name
+
+
 }
