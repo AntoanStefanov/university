8a2afb603f456f061477e14e86a8fc2e91564360
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-27 13:12:53 +0200

    inheritance structures

--- second_semester/inheritance/inheritance/Source.cpp
+++ second_semester/inheritance/inheritance/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <format>
 
+// https://stackoverflow.com/questions/2282725/c-what-is-for -> :: double colon
 using std::string;
 using std::cout;
 using std::endl;
@@ -10,6 +11,7 @@ class AbstractEmployee {
 	virtual void askForPromotion() = 0; // pure virtual function.
 };
 
+// https://cplusplus.com/forum/beginner/235722/ -> : single colon
 class Employee : AbstractEmployee {
 
 private:
@@ -72,19 +74,26 @@ public:
 // Employee -> parent class /base/superclass/
 // Developer -> child class /derived/subclass/
 
-class Developer : Employee {
+// In order to access the methods of the super class through Developer objects /ex. dev1.introduce(); /
+// we need to change the inheritance from private(default) to public.
+
+class Developer : public Employee {
 private:
 	// encapsulation
 	string programmingLanguage;
 public:
-	Developer(string devName, string devCompany, int devAge, string devProgrammingLanguage)
-		:Employee(devName, devCompany, devAge) // calling the super class constructor.
+	// https://cplusplus.com/forum/beginner/235722/ -> : single colon
+	// https://en.cppreference.com/w/cpp/language/constructor
+	// https://stackoverflow.com/questions/120876/what-are-the-rules-for-calling-the-base-class-constructor
+	// https://www.geeksforgeeks.org/when-do-we-use-initializer-list-in-c/
+	Developer(string devName, string devCompany, int devAge, string devProgrammingLanguage) : // <----- : (single colon)
+		Employee(devName, devCompany, devAge) // calling the super class constructor.
 	{
 		programmingLanguage = devProgrammingLanguage;
 	}
 
 	void fixBug() {
-		// accessing the protected prop, easily. Using getter for the private prop, otherwise -> error.
+		// accessing the protected prop, easily. Using getter to access the private prop "name", otherwise -> error.
 		cout << format("{} from {} company is fixing a bug.\n", getName(), company);
 	}
 
@@ -96,15 +105,58 @@ public:
 
 };
 
+// Another child class of Employee (C++ supports multiple inheritance)
+class Teacher : public Employee {
+private:
+	string subject;
+
+public:
+	// https://cplusplus.com/forum/beginner/235722/#msg1056281 -> : (single colon)
+	Teacher(string employeeName, string employeeCompany, int employeeAge, string teacherSubject) : // <-- single colon
+		Employee(employeeName, employeeCompany, employeeAge)
+	{
+		subject = teacherSubject;
+	}
+
+	void prepareLesson() {
+		cout << format("Lesson for {} prepared. By {}, working in {}.\n", subject, getName(), company);
+	}
+};
+
+
+class HistoryTeacher : public Teacher {
+private:
+	int lessonNumber = 1;
+
+public:
+	// https://cplusplus.com/forum/beginner/235722/#msg1056281 -> : (single colon)
+	HistoryTeacher(string employeeName, string employeeCompany, int employeeAge, string teacherSubject, int lessonNum) : // <-- single colon
+		Teacher(employeeName, employeeCompany, employeeAge, teacherSubject)
+	{
+		lessonNumber = lessonNum;
+	}
+
+	void showLessonNum() {
+		cout << format("Lesson number {} is ready, created by {}\n", lessonNumber, getName());
+	}
+};
+
 int main() {
 	Employee employee1 = Employee("John", "Company", 30);
 	employee1.introduce();
 	employee1.askForPromotion();
 
-	Developer employee2 = Developer("Tony", "Fruteli", 22, "JS");
-	employee2.fixBug();
+	Developer dev1 = Developer("Tony", "Fruteli", 22, "JS");
+	dev1.fixBug();
 
-	// employee2.introduce(); // | not working?
+	dev1.introduce(); // we have access to the method, bcs the inheritance is public.
+
+	Teacher teacher1 = Teacher("Rony", "Amazon", 32, "Software development");
+	teacher1.prepareLesson();
+	teacher1.askForPromotion();
+
+	HistoryTeacher hisotryTeacher1 = HistoryTeacher("Jack", "Amazon", 32, "Software development", 1);
+	hisotryTeacher1.showLessonNum();
 
 	// https://youtu.be/wN0x9eZLix4?t=3490
 	// Employee emp;// -> Error "no default constructor exists for class "Employee" "
