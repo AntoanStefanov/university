a249ddc60ada8683df1ca63b5517d088289e4aba
AntoanStefanov <antoine.stefanov@gmail.com>
2023-01-27 10:13:00 +0200

    virtual keyword

--- second_semester/virtual_keyword/virtual_keyword/Source.cpp
+++ second_semester/virtual_keyword/virtual_keyword/Source.cpp
@@ -6,6 +6,7 @@ using std::cout;
 using std::endl;
 using std::format;
 
+// https://www.simplilearn.com/tutorials/cpp-tutorial/virtual-function-in-cpp
 // https://www.geeksforgeeks.org/virtual-function-cpp/
 // https://stackoverflow.com/questions/2652198/difference-between-a-virtual-function-and-a-pure-virtual-function
 
@@ -40,13 +41,17 @@ public:
 
 int main()
 {
-	base* basePointer; // define pointer
+	base* basePointer; // define pointer with data type "base" class!
 	derived derivedClass;
-	basePointer = &derivedClass; // set pointer
+	basePointer = &derivedClass; // set pointer with data type "base", but value is derivedClass address/reference.
 
-	// Virtual function, binded at runtime
-	basePointer->print(); // this calls the overwritten method
+	// Virtual function, binded at runtime.
+	// And because it's virtual fn (in base), it calls the method on the pointer VALUE which is "derivedClass" reference.
+	// this calls the derived method.
+	basePointer->print();
 
 	// Non-virtual function, binded at compile time
-	basePointer->show(); // this calls the initial method
+	// Because it is NOT virtual(in base), it calls the method on the pointer DATA TYPE, which is "base".
+	// this calls the base method.
+	basePointer->show();
 }
