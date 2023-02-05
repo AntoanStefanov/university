8ee31ef23ea97b30edce0248efca5cfa7098c6f3
AntoanStefanov <antoine.stefanov@gmail.com>
2023-02-05 19:44:16 +0200

    DLL

--- second_semester/DDL_doubly_linked_list/DDL_doubly_linked_list/Source.cpp
+++ second_semester/DDL_doubly_linked_list/DDL_doubly_linked_list/Source.cpp
@@ -16,6 +16,15 @@ private:
 
 	public:
 		Node(int value, Node* previous, Node* next) : value(value), previous(previous), next(next) {}
+		explicit Node(int value) : value(value), previous(nullptr), next(nullptr) {}
+
+		Node* getPrevious() {
+			return previous;
+		}
+
+		Node* getNext() {
+			return previous;
+		}
 	};
 	int size = 0;
 	Node* head = nullptr;
@@ -33,13 +42,52 @@ public:
 		return size;
 	}
 
-	Node* addNode() {
+	bool isEmpty() const {
+		return getSize() == 0;
+	}
 
+	Node* addNode(int value) {
+		Node* node = new Node(value);
+		if (isEmpty()) {
+			head = tail = node;
+		}
+
+		return node;
+	}
+
+	Node* addNode(int value, Node* previous, Node* next) {
+		Node* node = new Node(value);
+		head = tail = node;
+		return node;
+	}
+
+
+};
+
+// https://www.programiz.com/cpp-programming/class-templates
+template <class T>
+class Number {
+private:
+	// Variable of type T
+	T num;
+
+public:
+	explicit Number(T n) : num(n) {}   // constructor
+
+	T getNum() {
+		return num;
 	}
 };
 
-
 int main() {
+	// create object with int type
+	Number<int> numberInt = Number(7);
+	numberInt.getNum();
+	// create object with double type
+	Number<double> numberFloat = Number(7.7);
+	numberFloat.getNum();
+
+
 	DoublyLinkedList DLL;
 	DLL.getHead();
 	DLL.getSize();
