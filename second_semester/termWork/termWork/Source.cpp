d5be4393c9b687f976127f75f003d8b0ad4c4ff5
AntoanStefanov <antoine.stefanov@gmail.com>
2023-04-01 23:51:13 +0300

    modifying base class to be an abstract class with ppure virtual fns.

--- second_semester/termWork/termWork/Source.cpp
+++ second_semester/termWork/termWork/Source.cpp
@@ -6,23 +6,12 @@ using std::cout;
 using std::endl;
 using std::format;
 
+// https://www.ibm.com/docs/en/zos/2.4.0?topic=only-abstract-classes-c ? class Semiconductor
+// https://stackoverflow.com/questions/8513408/c-abstract-base-class-constructors-destructors-general-correctness
 class Semiconductor {
 public:
-	Semiconductor();
-	Semiconductor(string const&, int);
-	Semiconductor(Semiconductor const&);
-	~Semiconductor();
-	void showInfo();
-
-	void setModel(const string&);
-	string getModel() const;
-
-	void setYear(int);
-	int getYear() const;
-
-private:
-	string model;
-	int year;
+	virtual ~Semiconductor() = 0; // pure virtual function, ... = 0
+	virtual void showInfo() = 0; // pure virtual function, ... = 0
 };
 
 class Diode : public Semiconductor {
@@ -37,6 +26,8 @@ public:
 
 	void setForwardVolts(int);
 	double getForwardVolts() const;
+
+	virtual void showInfo();
 
 private:
 	string chemicalElement; // silicon, germanium, etc...
@@ -98,47 +89,15 @@ private:
 };
 
 int main() {
-	Semiconductor sem1("RR2", 2023);
-	Semiconductor semCopy(sem1);
-	sem1.showInfo();
-	semCopy.showInfo();
+	cout << "main" << endl;
+	Diode diode;
 }
 
-Semiconductor::Semiconductor() {
-	model = "UNDEFINED";
-	year = 0000;
-}
-
-Semiconductor::Semiconductor(const string& semiconductorModel, int semiconductorYear) {
-	model = semiconductorModel;
-	year = semiconductorYear;
-}
-
-Semiconductor::Semiconductor(Semiconductor const& semiconductorObject) {
-	model = semiconductorObject.model;
-	year = semiconductorObject.year;
-};
 
 Semiconductor::~Semiconductor() {
 	cout << "Semiconductor Destructor" << endl;
 }
 
 void Semiconductor::showInfo() {
-	cout << format("Semiconductor: {}-{}", model, year) << endl;
-}
-
-void Semiconductor::setModel(const string& semiconductorModel) {
-	model = semiconductorModel;
-}
-
-string Semiconductor::getModel() const {
-	return model;
-}
-
-void Semiconductor::setYear(const int semiconductorYear) {
-	year = semiconductorYear;
-}
-
-int Semiconductor::getYear() const {
-	return year;
+	cout << "Semiconductor: {}-{}" << endl;
 }
